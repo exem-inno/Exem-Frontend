eec6e7bc6c13a7745fcd808e8796e74f0aa2db9d
강명구 <mg5566@hanmail.net>
2022-04-06 16:26:36 +0900

    feat: apply page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="*" element={<LoginPage />} />
+    </Routes>
+  );
 }
 
 export default App;
