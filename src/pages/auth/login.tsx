4b24a8c89985d792697cde141720c5d8a92e49e3
mg5566 <mg5566@hanmail.net>
2022-04-06 17:29:32 +0900

    Auth Page 구현 (#3)
    * init setting
    
    * feat: add mui icons package
    
    * feat: add react router package
    
    * feat: create login page
    
    * feat: apply page route
    
    * refactor: modify route path
    
    * refactor: modify file name and apply useCallback
    
    * refactor: modify title
    
    * refactor: modify struct of pages directory
    
    * refactor: modify button phrase

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,48 @@
+import React, { useCallback } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+
+const LoginPage: React.VFC = () => {
+  const handleSubmit = useCallback((event: React.FormEvent): void => {
+    event.preventDefault();
+    console.log("Clicked Sign In Button");
+  }, []);
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Log In
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Log In with Google
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default LoginPage;
