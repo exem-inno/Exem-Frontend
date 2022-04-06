2806a6edcbbe9e32e1fcb56204604de1735be753
강명구 <mg5566@hanmail.net>
2022-04-06 16:57:01 +0900

    refactor: modify route path

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/login";
 
 function App() {
   return (
     <Routes>
-      <Route path="*" element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
