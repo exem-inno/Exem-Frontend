ff5fe170ba5607a9913391d6616104b56210e778
jiwlee <ddazi9576@google.com>
2022-04-07 20:50:55 +0900

    feat: dashboardpage route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/auth/login";
+import DashBoardPage from "./pages/dashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/auth/login" element={<LoginPage />} />
+      <Route path="/dashboard" element={<DashBoardPage />} />
     </Routes>
   );
 }
