cdf641699533f7dc9663abc79b9860d206d41c96
강명구 <mg5566@hanmail.net>
2022-04-07 15:48:30 +0900

    feat: add new chart page for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/auth/login";
+import NewChartPage from "./pages/chart/newChart";
 
 function App() {
   return (
     <Routes>
       <Route path="/auth/login" element={<LoginPage />} />
+      <Route path="/chart/newChart" element={<NewChartPage />} />
     </Routes>
   );
 }
