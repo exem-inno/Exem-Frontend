6038aaa6039d485fa75a487bf2012965f41cfedc
jiwlee <ddazi9576@google.com>
2022-04-07 23:14:49 +0900

    refactor: ThemeProvider 삭제

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,21 +1,16 @@
 import { VFC } from "react";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import AppBar from "components/dashboard/AppBar";
 import Dashboard from "components/dashboard/Dashboard";
 
-const mdTheme = createTheme();
-
 const DashboardPage: VFC = () => {
   return (
-    <ThemeProvider theme={mdTheme}>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        <AppBar />
-        <Dashboard />
-      </Box>
-    </ThemeProvider>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar />
+      <Dashboard />
+    </Box>
   );
 };
 
