36a5090721874789fb29cd0d520b5a5975386c93
강명구 <mg5566@hanmail.net>
2022-04-08 18:27:00 +0900

    refactor: remove submit handler

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -7,24 +7,12 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
+const url = "http://localhost:8080";
+const redirectionURL = "http://localhost:3000/auth/handle";
+const loginURL =
+  url + "/oauth2/authorization/google?redirection_url=" + redirectionURL;
+
 const LoginPage: React.VFC = () => {
-  const handleSubmit = useCallback((event: React.FormEvent): void => {
-    event.preventDefault();
-    console.log("Clicked Sign In Button");
-
-    // Google Login
-    const url = "http://localhost:8080";
-    const redirectionURL = "http://localhost:3000/auth/handle";
-    fetch(url + "/oauth2/authorization/google?redirection_url=" + redirectionURL)
-    .then((res) => {
-      if (res.ok){
-        return res.json();
-      }
-    }).then((data) => {
-      console.log("login success", data);
-    });
-  }, []);
-
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -42,12 +30,13 @@ const LoginPage: React.VFC = () => {
         <Typography component="h1" variant="h5">
           Log In
         </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <Box component="form" noValidate sx={{ mt: 1 }}>
           <Button
-            type="submit"
             fullWidth
             variant="contained"
             sx={{ mt: 3, mb: 2 }}
+            // login
+            href={loginURL}
           >
             Log In with Google
           </Button>
