89d331d2e760e020e33bcfc772666feaaf6e1fa4
강명구 <mg5566@hanmail.net>
2022-04-08 16:07:54 +0900

    feat: add handle routing path

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
+import HandlePage from "./pages/auth/handle";
 import LoginPage from "./pages/auth/login";
 
 function App() {
   return (
     <Routes>
       <Route path="/auth/login" element={<LoginPage />} />
+      <Route path="/auth/handle" element={<HandlePage />} />
     </Routes>
   );
 }
