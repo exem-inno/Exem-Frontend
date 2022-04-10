b674833d60a52a5bedfd6da6c995b352454bcf9f
jiwlee <ddazi9576@google.com>
2022-04-11 01:22:16 +0900

    feat: Dashboard component에 ResponsiveGridLayout 적용

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,7 +1,38 @@
-import { VFC } from "react";
+import { useState, VFC } from "react";
 import Box from "@mui/material/Box";
+import { Responsive, WidthProvider } from "react-grid-layout";
+import Toolbar from "@mui/material/Toolbar";
+import _ from "lodash";
+import "./dashboard.css";
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
+function generateLayout() {
+  return _.map(_.range(0, 10), function (item, i) {
+    var y = Math.ceil(Math.random() * 4) + 1;
+    return {
+      x: (_.random(0, 5) * 2) % 12,
+      y: Math.floor(i / 6) * y,
+      w: 2,
+      h: y,
+      i: i.toString(),
+    };
+  });
+}
 
 const Dashboard: VFC = () => {
+  const [layouts, setLayouts] = useState({ lg: generateLayout() });
+
+  const generateDOM = () => {
+    return _.map(layouts.lg, function (l, i) {
+      return (
+        <div key={i}>
+          <span className="text">{i}</span>
+        </div>
+      );
+    });
+  };
+
   return (
     <Box
       component="main"
@@ -14,7 +45,19 @@ const Dashboard: VFC = () => {
         height: "100vh",
         overflow: "auto",
       }}
-    ></Box>
+    >
+      <Toolbar />
+      <ResponsiveGridLayout
+        className="layout"
+        layouts={layouts}
+        breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+        rowHeight={30}
+        width={1200}
+      >
+        {generateDOM()}
+      </ResponsiveGridLayout>
+    </Box>
   );
 };
 
