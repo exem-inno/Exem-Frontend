20251bcea6165d50fff537fd14872dbce3cdf9a2
강명구 <mg5566@hanmail.net>
2022-04-11 23:35:07 +0900

    feat: apply add new chart modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "@mui/material";
+import { Box } from "@mui/system";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/auth/login";
-import NewChartPage from "./pages/new-chart/newChart";
+import CustomModal from "./components/modal/CustomModal";
+
+const MainPage: React.VFC = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <Box>
+      <Button onClick={handleOpen}>open modal</Button>
+      <CustomModal open={open} onClose={handleClose} />
+    </Box>
+  );
+};
 
 function App() {
   return (
     <Routes>
       <Route path="/auth/login" element={<LoginPage />} />
-      <Route path="/new-chart/newChart" element={<NewChartPage />} />
+      <Route path="/" element={<MainPage />} />
     </Routes>
   );
 }
