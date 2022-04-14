54e7f1304361fe4ec0bc65438885bc1dd3a5fcac
강명구 <mg5566@hanmail.net>
2022-04-14 18:21:12 +0900

    refactor: modify auth logic

--- src/pages/auth/handle.tsx
+++ src/pages/auth/handle.tsx
@@ -1,41 +1,32 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+// backend path
+const url =
+  "http://ec2-13-209-251-220.ap-northeast-2.compute.amazonaws.com:8080";
 
 const HandlePage: React.VFC = () => {
-  const location = useLocation();
-  const [redirectionPage, setRedirectionPage] = useState<string>("/");
-
-  const getUrlParameter = useCallback(
-    (name: string) => {
-      var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
-
-      var results = regex.exec(location.search);
-      return results === null
-        ? ""
-        : decodeURIComponent(results[1].replace(/\+/g, " "));
-    },
-    [location]
-  );
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const token = getUrlParameter("token");
-    if (token) {
-      localStorage.setItem("accessToken", token);
-      setRedirectionPage("/");
-    } else {
-      // TODO: return 전에 반영시켜야함....
-      setRedirectionPage(() => "/auth/login");
-    }
-  }, [getUrlParameter]);
+    fetch(url + "/user/me")
+      .then((res) => {
+        if (!res.ok) {
+          return Promise.reject(res);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log("user data", data);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log("/user/me Error", err);
+        // navigate("/auth/login");
+      });
+  }, [navigate]);
 
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Navigate replace={true} to={`${redirectionPage}`} />}
-      />
-    </Routes>
-  );
+  return <p>로그인 시도 중 잠시만 기다리시오...</p>;
 };
 
 export default HandlePage;
