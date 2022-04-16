716a96fe95dc8712f386a5b20d9c0781e836b186
jiwlee <ddazi9576@google.com>
2022-04-16 15:46:25 +0900

    feat: DashboardLayout 작성

---
+++ src/layouts/dashboard/index.tsx
@@ -0,0 +1,41 @@
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import AppBar from "components/dashboard/layout/AppBar";
+import Drawer from "components/dashboard/layout/Drawer";
+import GridLayout from "components/dashboard/layout/GridLayout";
+import { FC, useState, useCallback } from "react";
+
+export const drawerWidth: number = 180;
+
+const DashboardLayout: FC = ({ children }) => {
+  const [open, setOpen] = useState(false);
+  const toggleDrawer = useCallback(() => {
+    setOpen((prev) => !prev);
+  }, []);
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar open={open} toggleDrawer={toggleDrawer} />
+      <Drawer open={open} toggleDrawer={toggleDrawer} />
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light"
+              ? theme.palette.grey[100]
+              : theme.palette.grey[900],
+          flexGrow: 1,
+          height: "100vh",
+          overflow: "auto",
+        }}
+      >
+        <Toolbar />
+        <GridLayout>{children}</GridLayout>
+      </Box>
+    </Box>
+  );
+};
+
+export default DashboardLayout;
