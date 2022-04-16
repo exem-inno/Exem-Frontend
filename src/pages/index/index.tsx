06ade78f5b49edcb61222124afc3f40925b78408
강명구 <mg5566@hanmail.net>
2022-04-16 17:14:59 +0900

    new logic

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,13 +2,25 @@ import React, { useEffect } from "react";
 
 const MainPage: React.VFC = () => {
   useEffect(() => {
+    // fetch("http://localhost:8080/user/me", {
+    //   redirect: "follow",
+    //   credentials: "include",
+    // })
+    // fetch("/user/me", {
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   credentials: "include",
+    //   redirect: "follow",
+    // })
     fetch("http://localhost:8080/user/me", {
-      redirect: "follow",
       credentials: "include",
+      // redirect: "follow",
     })
       .then((res) => {
         if (res.redirected) {
           console.log("res.url", res.url);
+          // document.location = res.url;
         }
         return res.json();
       })
