9a7f5f46c6fe15a8601c5d9c88441d9417b8363a
jiwlee <ddazi9576@google.com>
2022-04-16 15:49:52 +0900

    feat: App에 dashbord/* route 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import DashboardPage from "pages/dashboard";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/auth/login";
-import DashBoardPage from "./pages/dashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/auth/login" element={<LoginPage />} />
-      <Route path="/dashboard" element={<DashBoardPage />} />
+      <Route path="/dashboard/*" element={<DashboardPage />} />
     </Routes>
   );
 }
