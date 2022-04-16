1dc1929e4fc8f85e837df0525b37f430bd2d522c
jiwlee <ddazi9576@google.com>
2022-04-16 15:45:43 +0900

    feat: 폴더 정리

---
+++ src/components/dashboard/layout/AppBar.tsx
@@ -0,0 +1,71 @@
+import { VFC } from "react";
+import { styled } from "@mui/material/styles";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import AccountCircleSharpIcon from "@mui/icons-material/AccountCircleSharp";
+import MenuIcon from "@mui/icons-material/Menu";
+import { drawerWidth } from "layouts/dashboard";
+
+interface StyledAppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const StyledAppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<StyledAppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+interface Props {
+  open: boolean;
+  toggleDrawer: () => void;
+}
+
+const AppBar: VFC<Props> = ({ open, toggleDrawer }) => {
+  return (
+    <StyledAppBar position="absolute" open={open}>
+      <Toolbar sx={{ pr: "24px" }}>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          onClick={toggleDrawer}
+          sx={{
+            marginRight: "36px",
+            ...(open && { display: "none" }),
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          component="h1"
+          variant="h6"
+          color="inherit"
+          noWrap
+          sx={{ flexGrow: 1 }}
+        >
+          Dashboard
+        </Typography>
+        <IconButton color="inherit">
+          <AccountCircleSharpIcon />
+        </IconButton>
+      </Toolbar>
+    </StyledAppBar>
+  );
+};
+
+export default AppBar;
