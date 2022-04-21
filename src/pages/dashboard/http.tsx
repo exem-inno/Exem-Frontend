d6d113c159b6119369d92858e8458543d93d2d29
jiwlee <ddazi9576@google.com>
2022-04-21 14:46:48 +0900

    feat: onClickRow event handler 추가

--- src/pages/dashboard/http.tsx
+++ src/pages/dashboard/http.tsx
@@ -1,11 +1,13 @@
 import GridLayout from "components/dashboard/layout/GridLayout";
 import HttpData from "components/tables/HttpData";
 import _ from "lodash";
-import { useEffect, useState, VFC } from "react";
+import { useCallback, useEffect, useState, VFC } from "react";
 import { ILayout } from "types/layout";
 import rows from "datas/httpdata.json";
+import Span from "components/charts/Span";
 
 const HTTPPage: VFC = () => {
+  const [selectedRow, setSelectedRow] = useState<number | null>(null);
   const [layouts, setLayouts] = useState<{ lg: ILayout[] }>({
     lg: [
       {
@@ -19,6 +21,46 @@ const HTTPPage: VFC = () => {
     ],
   });
 
+  const onClickRow = useCallback(
+    (event, ind) => {
+      if (ind === selectedRow) {
+        setSelectedRow(null);
+        setLayouts({
+          lg: [
+            {
+              i: "0",
+              x: 0,
+              y: 0,
+              w: 12,
+              h:
+                4 +
+                3 * Math.floor((rows.length - 1) / 2) +
+                (rows.length % 2 ? 2 : 3),
+            },
+          ],
+        });
+      } else {
+        setSelectedRow(ind);
+        setLayouts({
+          lg: [
+            {
+              i: "0",
+              x: 0,
+              y: 0,
+              w: 12,
+              h:
+                4 +
+                3 * Math.floor((rows.length - 1) / 2) +
+                (rows.length % 2 ? 2 : 3),
+            },
+            { i: "1", x: 0, y: 0, w: 12, h: 9 },
+          ],
+        });
+      }
+    },
+    [selectedRow]
+  );
+
   useEffect(() => {
     const height =
       4 + 3 * Math.floor((rows.length - 1) / 2) + (rows.length % 2 ? 2 : 3);
@@ -31,7 +73,8 @@ const HTTPPage: VFC = () => {
     return _.map(layouts.lg, function (l, i) {
       return (
         <div key={i} style={{ overflow: "scroll" }}>
-          <HttpData />
+          {i === 0 && <HttpData onClickRow={onClickRow} />}
+          {i === 1 && <Span title="Span Data" />}
         </div>
       );
     });
