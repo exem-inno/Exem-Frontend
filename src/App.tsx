27d4e718641e2c994354402176760802365c059e
강명구 <mg5566@hanmail.net>
2022-04-21 17:35:29 +0900

    fix: remove dev deps

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,12 @@ import ServicePage from "./pages/services";
 
 function App() {
   return (
-    <div>
-      <div>
-        <a href="/services/1">1번 서비스로 이동</a>
-      </div>
-      <div>
-        <a href="/services/2">2번 서비스로 이동</a>
-      </div>
-      <div>
-        <a href="/services/3">3번 서비스로 이동</a>
-      </div>
-      <div>
-        <a href="/services/4">4번 서비스로 이동</a>
-      </div>
-      <Routes>
-        <Route path="/auth/login" element={<LoginPage />} />
-        {/* <Route path="/" element={<MainPage />} /> */}
-        <Route path="/services/:serviceId/*" element={<ServicePage />} />
+    <Routes>
+      <Route path="/auth/login" element={<LoginPage />} />
+      {/* <Route path="/" element={<MainPage />} /> */}
+      <Route path="/services/:serviceId/*" element={<ServicePage />} />
       <Route path="/dashboard/*" element={<DashboardPage />} />
-      </Routes>
-    </div>
+    </Routes>
   );
 }
 
