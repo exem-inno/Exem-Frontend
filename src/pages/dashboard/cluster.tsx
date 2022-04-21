146ce2f4f814304e2908784341ba430b95b2a4a1
jiwlee <ddazi9576@google.com>
2022-04-21 17:23:56 +0900

    refactor: GridItem 적용

--- src/pages/dashboard/cluster.tsx
+++ src/pages/dashboard/cluster.tsx
@@ -1,39 +1,29 @@
-import Graph from "components/charts/Graph";
-import { useState, VFC } from "react";
-import data from "datas/graph.json";
-import NodesTable from "components/tables/Nodes";
-import _ from "lodash";
+import { useEffect, useState, VFC } from "react";
 import GridLayout from "components/dashboard/layout/GridLayout";
-import NamespacesTable from "components/tables/Namespaces";
 import { ILayout } from "types/layout";
+import GridItem from "components/dashboard/layout/GridItem";
+import _ from "lodash";
 
 const ClusterPage: VFC = () => {
-  const [layouts] = useState<{ lg: ILayout[] }>({
-    lg: [
-      { i: "0", x: 0, y: 0, w: 12, h: 12 },
-      { i: "1", x: 0, y: 0, w: 6, h: 12 },
-      { i: "2", x: 6, y: 0, w: 6, h: 12 },
-    ],
+  const [layouts, setLayouts] = useState<{ lg: ILayout[] }>({
+    lg: [],
   });
 
-  const options = {
-    nodes: {
-      shape: "dot",
-    },
-    edges: {
-      arrows: "to",
-    },
-  };
+  useEffect(() => {
+    setLayouts({
+      lg: [
+        { i: "service-graph", x: 0, y: 0, w: 12, h: 12 },
+        { i: "nodes-table", x: 0, y: 0, w: 6, h: 12 },
+        { i: "namespaces-table", x: 6, y: 0, w: 6, h: 12 },
+      ],
+    });
+  }, []);
 
   const generateDOM = () => {
-    return _.map(layouts.lg, function (l, i) {
+    return _.map(layouts.lg, function (l) {
       return (
-        <div key={i} style={{ overflow: "scroll" }}>
-          {i === 0 && (
-            <Graph title="Service Graph" data={data} options={options} />
-          )}
-          {i === 1 && <NodesTable />}
-          {i === 2 && <NamespacesTable />}
+        <div key={l.i} style={{ overflow: "scroll" }}>
+          <GridItem key={l.i} type={l.i} />
         </div>
       );
     });
