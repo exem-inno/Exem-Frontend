a17c87e24db6a4e4a620a5dfd384824cb2d78262
jiwlee <ddazi9576@google.com>
2022-04-22 14:45:06 +0900

    feat:Services 컴포넌트에 useQuery 적용

--- src/components/charts/Services.tsx
+++ src/components/charts/Services.tsx
@@ -1,8 +1,12 @@
 import Graph from "./Graph";
-import data from "datas/graph.json";
-import { VFC } from "react";
+import { useEffect, useState, VFC } from "react";
+import { useQuery } from "react-query";
+import { fetchJson } from "lib/api";
+import { IEdge, INode } from "types/graph";
 
 const ServicesGraph: VFC = () => {
+  const [nodes, setNodes] = useState<INode[]>([]);
+  const [edges, setEdges] = useState<IEdge[]>([]);
   const options = {
     nodes: {
       shape: "dot",
@@ -11,7 +15,55 @@ const ServicesGraph: VFC = () => {
       arrows: "to",
     },
   };
-  return <Graph title="Service Graph" data={data} options={options} />;
+
+  const { data } = useQuery("service-graph-data", async () => {
+    try {
+      return await fetchJson("/api/fromTo");
+    } catch (err) {
+      return undefined;
+    }
+  });
+  useEffect(() => {
+    console.log("data updated!", data);
+    if (data) {
+      const services = new Set<string>();
+      setEdges(
+        data.map(
+          (v: { from: string; to: string; count: number }, i: number) => {
+            services.add(v.from);
+            services.add(v.to);
+            return {
+              id: i,
+              from: v.from,
+              to: v.to,
+            };
+          }
+        )
+      );
+      if (services) {
+        setNodes(
+          Array.from(services).map((service: string) => ({
+            id: service,
+            label: service,
+          }))
+        );
+      }
+    }
+  }, [data]);
+  return (
+    <>
+      {data ? (
+        <Graph
+          title="Service Graph"
+          nodes={nodes}
+          edges={edges}
+          options={options}
+        />
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
+  );
 };
 
 export default ServicesGraph;
